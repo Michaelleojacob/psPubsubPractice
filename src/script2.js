a5f88ef8f95d1d8ece2f531a82b6f5c6e5986358
Michael <michaelleojacob@gmail.com>
2021-11-19 19:23:13 -0800

    put the current script logic into script2.js, moved the people adder logic and exmaple to the submit file in the testingSubs folder and now am importing it.

---
+++ src/script2.js
@@ -0,0 +1,48 @@
+import './style.css';
+
+// import testingShowMeTheMoney from './testingSubs/testingShowMeTheMoney';
+// testingShowMeTheMoney();
+
+// import wrapAllIncrementNumberLogic from './testingSubs/incNum';
+// wrapAllIncrementNumberLogic();
+
+import cached from './cacheDom/cacheDom';
+import ps from './Pubsub/Pubsub';
+
+const { form, input, counter, people } = cached;
+
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) parent.removeChild(parent.firstChild);
+};
+const peopleArr = ['mike', 'sam', 'pedro'];
+
+form.addEventListener('submit', (e) => e.preventDefault());
+form.addEventListener('submit', () => {
+  peopleArr.push(input.value);
+  input.value = '';
+  ps.publish('personUpdated', peopleArr);
+});
+
+const render = (arr) => {
+  removeAllChildNodes(people);
+  arr.map((x) => {
+    const element = document.createElement('div');
+    element.textContent = x;
+    element.classList.add('deleteMe');
+    return people.appendChild(element);
+  });
+  counter.textContent = arr.length;
+};
+
+const handleDeleteMe = (e) => {
+  if (e.target.classList.contains('deleteMe')) {
+    peopleArr.splice(peopleArr.indexOf(e.target.textContent), 1);
+    ps.publish('personUpdated', peopleArr);
+  }
+};
+
+people.addEventListener('click', handleDeleteMe);
+
+ps.subscribe('personUpdated', render);
+
+render(peopleArr);
---
+++ src/testingSubs/submit.js
@@ -0,0 +1,44 @@
+import cached from '../cacheDom/cacheDom';
+import ps from '../Pubsub/Pubsub';
+
+const wrapAllSubmitLogic = () => {
+  const { form, input, counter, people } = cached;
+
+  const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
+  };
+  const peopleArr = ['mike', 'sam', 'pedro'];
+
+  form.addEventListener('submit', (e) => e.preventDefault());
+  form.addEventListener('submit', () => {
+    peopleArr.push(input.value);
+    input.value = '';
+    ps.publish('personUpdated', peopleArr);
+  });
+
+  const render = (arr) => {
+    removeAllChildNodes(people);
+    arr.map((x) => {
+      const element = document.createElement('div');
+      element.textContent = x;
+      element.classList.add('deleteMe');
+      return people.appendChild(element);
+    });
+    counter.textContent = arr.length;
+  };
+
+  const handleDeleteMe = (e) => {
+    if (e.target.classList.contains('deleteMe')) {
+      peopleArr.splice(peopleArr.indexOf(e.target.textContent), 1);
+      ps.publish('personUpdated', peopleArr);
+    }
+  };
+
+  people.addEventListener('click', handleDeleteMe);
+
+  ps.subscribe('personUpdated', render);
+
+  render(peopleArr);
+};
+
+export default wrapAllSubmitLogic;
